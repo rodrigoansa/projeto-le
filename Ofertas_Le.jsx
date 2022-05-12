5bad182341e07acdb3612b89f2e86859517f6fb6
Rodrigo Ansa <rodrigoansa2@gmail.com>
2022-05-12 18:55:28 -0300

    alteração 12/05

--- Ofertas_Le.jsx
+++ Ofertas_Le.jsx
@@ -1,7 +1,37 @@
 //Criar Janela principal.
+var MyJanela = new Window("palette", "Ofertas Le", undefined);
+//var titulo = MyJanela.add("statictext", undefined, "OFERTAS LE");
+MyJanela.orientation = "column";
+
+var PegandoMarca = File("imagensLe/ofertasLe.png");
+var Marca = MyJanela.add("iconbutton", undefined, PegandoMarca, {style: "toolbutton"});
+
+var SubTituloOne = MyJanela.add("statictext", undefined, "DESCRIÇÃO");
+
 
 //Escrever nome dos produto 1, produto 2, Marca e referência.
+var textGroup = MyJanela.add("group", undefined, "");
+textGroup.orientation = "column";
+textGroup.size = [300, 600];
+
+var produtoOne = textGroup.add("edittext", undefined,"Produto 1");
+var produtoTwo = textGroup.add("edittext", undefined, "Produto 2");
+var marca = textGroup.add("edittext", undefined, "Marca");
+var referencia = textGroup.add("edittext", undefined, "Referências");
+
+//Definindo o tamanho das caixas de textos.
+produtoOne.size = [180, 25];
+produtoTwo.size = [180, 25];
+marca.size = [180, 25];
+referencia.size = [180, 25];
+
 // Criar botão aplicar para cada um.
+var grupoBotao = MyJanela.add("group", undefined, "grupoBotao");
+grupoBotao.orientation = "row";
+
+var buttonOne = grupoBotao.add("button", undefined, "Aplicar");
+
+
 
 //Checkbox se vai ter tags, se tiver abre espaço para adiciona-las.
 //Radio button para selecionar o número de tags de 1 a 4
@@ -43,3 +73,5 @@
 //Escrever valor à vista.
 
 //Encerrar
+MyJanela.center(); 
+MyJanela.show();
