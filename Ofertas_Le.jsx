2b9114c7fcefaeb97faf0d338da3929ad01cd8b7
Rodrigo Ansa <rodrigoansa2@gmail.com>
2022-05-12 16:10:31 -0300

    Create Ofertas_Le.jsx

---
+++ Ofertas_Le.jsx
@@ -0,0 +1,45 @@
+//Criar Janela principal.
+
+//Escrever nome dos produto 1, produto 2, Marca e referência.
+// Criar botão aplicar para cada um.
+
+//Checkbox se vai ter tags, se tiver abre espaço para adiciona-las.
+//Radio button para selecionar o número de tags de 1 a 4
+//Escrever nome tag 1, tag 2, tag, 3 e tag 4
+
+//Radiobutton para selecionar o tipo de ofertas:
+//Por, De por, De por parcelado, À partir de, À vista parcelado, Parcelado principal de por, Parcelado principal à vista.
+
+//Por:
+//Escrever valor Por.
+
+//De por:
+//Escrever valor de.
+//Escrever valor por.
+
+//De por parcelado:
+//Escrever valor de.
+//Escrever valor por.
+//Escrever parcela.
+//Escrever valor parcela.
+
+//À partir de:
+//Escrever valor de.
+
+//À vista parcelado:
+//Escrever valor à vista.
+//Escrever parcela.
+//Escrever valor parcela.
+
+//Parcelado principal de por:
+//Escrever parcela.
+//Escrever valor parcela.
+//Escrever valor de.
+//Escrever valor por.
+
+//Parcelado principal à vista:
+//Escrever parcelado.
+//Escrever valor parcela.
+//Escrever valor à vista.
+
+//Encerrar
